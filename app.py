cd5cbc5a838a620588173ecd9e6a1541de2df830
Carlos Rodrigues <carlosrodriguesjf@gmail.com>
2023-05-19 22:50:36 -0300

    Projeto completo

---
+++ app.py
@@ -0,0 +1,23 @@
+# PROJETO #5 - MESTRES DA AUTOMAÇÃO - CRIE UM BOT QUE BAIXE ARQUIVOS POR VOCÊ
+
+import webbrowser
+import pyautogui
+from time import sleep
+
+webbrowser.open('https://file-examples.com/')
+
+sleep(3)
+
+pyautogui.click(3974,865,duration=2)
+sleep(2)
+pyautogui.click(4268,1099,duration=2)
+sleep(2)
+pyautogui.click(4558,924,duration=2)
+sleep(2)
+pyautogui.click(4604,1030,duration=2)
+sleep(3)
+pyautogui.click(4763,954,duration=2)
+sleep(2)
+pyautogui.click(4645,833,duration=2)
+sleep(2)
+pyautogui.click(4591,628,duration=2)
